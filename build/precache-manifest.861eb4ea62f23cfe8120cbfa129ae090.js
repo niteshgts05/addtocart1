7aed6f63d81850d509996e16005eeaada68414ef
niteshgts05 <mernstack05@gmail.com>
2021-09-21 09:22:16 +0530

    build updated !

---
+++ build/precache-manifest.861eb4ea62f23cfe8120cbfa129ae090.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16079d0f83ef7b50f5dae8d911d4e9b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78f52a87fa1ffdcfe0e4",
+    "url": "/static/css/2.e62bb353.chunk.css"
+  },
+  {
+    "revision": "cc2420e530b5c45b226c",
+    "url": "/static/css/main.27d029fe.chunk.css"
+  },
+  {
+    "revision": "78f52a87fa1ffdcfe0e4",
+    "url": "/static/js/2.6c169907.chunk.js"
+  },
+  {
+    "revision": "97557dac625cf0680a71c54f9bece515",
+    "url": "/static/js/2.6c169907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc2420e530b5c45b226c",
+    "url": "/static/js/main.61be3feb.chunk.js"
+  },
+  {
+    "revision": "71e0677ab2f28215227f",
+    "url": "/static/js/runtime-main.b234ba30.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- src/app/components/web/views/home/index.js
+++ src/app/components/web/views/home/index.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from "react";
+import React from "react";
 import Bannerslider from "../banner-carousel";
 import Topsavers from "./top-section";
 
